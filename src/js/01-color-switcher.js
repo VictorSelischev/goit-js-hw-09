866cb63928b6e91966094b57809b4fee23ef5e82
Victor Selischev <selischev.victor.kharkov@gmail.com>
2022-07-30 19:06:39 +0300

    resolve color-switcher - HW9-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
 
+let timerId = null;
+
+startButton.addEventListener('click', handleChangeColorBodyClick);
+
+stopButton.addEventListener('click', handleStopColorBodyClick);
+
+
+
+// ==============================
+// FUNCTION
+
+function handleChangeColorBodyClick() {
+    timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+    startButton.disabled = true;
+    stopButton.disabled = false;
+}
+
+function handleStopColorBodyClick() {
+    clearInterval(timerId);
+    startButton.disabled = false;
+    stopButton.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
