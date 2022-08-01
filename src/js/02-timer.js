5374033c7e9c11baf71e3e684bb3b967d98cd3a5
Victor Selischev <selischev.victor.kharkov@gmail.com>
2022-08-01 18:21:27 +0300

    Оформляет элементы интерфейса таймера

---
+++ src/js/02-timer.js
@@ -0,0 +1,25 @@
+const timerRef = document.querySelector('.timer');
+const fieldRef = document.querySelectorAll('.field');
+const valueRef = document.querySelectorAll('.value');
+const labelRef = document.querySelectorAll('.label');
+
+timerRef.style.display = 'flex';
+timerRef.style.marginTop = '40px';
+
+fieldRef.forEach(el => {
+    el.style.display = 'flex';
+    el.style.flexDirection = 'column';
+    el.style.alignItems = 'center';
+    el.style.marginRight = '20px';
+})
+
+valueRef.forEach(el => {
+    el.style.fontSize = '50px';
+    el.style.fontFamily = "Roboto, sans-serif";
+})
+
+labelRef.forEach(el => {
+    el.style.textTransform = 'uppercase';
+    el.style.fontSize = '16px';
+    el.style.fontFamily = "Roboto, sans-serif";
+})
