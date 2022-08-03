94d35dc937e7899e1ed468a2de02bc82a8bc8821
Victor Selischev <selischev.victor.kharkov@gmail.com>
2022-08-03 11:12:11 +0300

    Добавляет библиотеку flatpickr в задание 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,12 +1,39 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
 inlineStyleTimer();
 
+let differentTime = null;
 
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (new Date() >= selectedDates[0]) {
+      dataButton.disabled = true;
+      window.alert('Please choose a date in the future');
+      return;
+    }
+
+    dataButton.disabled = false;
+    differentTime = selectedDates[0].getTime() - Date.now();
+    
+    dataButton.addEventListener('click', handleShowClockClick);
+  },
+};
+
+const dateTimePicker = document.querySelector('#datetime-picker');
+const dataButton = document.querySelector('button[data-start]');
+dataButton.disabled = true;
+
+flatpickr(dateTimePicker, options);
 
 // =========================================
 // FUNCTION
 
 function inlineStyleTimer() {
-
   const timerRef = document.querySelector('.timer');
   const fieldRef = document.querySelectorAll('.field');
   const valueRef = document.querySelectorAll('.value');
@@ -32,8 +59,38 @@ function inlineStyleTimer() {
     el.style.fontSize = '16px';
     el.style.fontFamily = 'Roboto, sans-serif';
   });
-    
 }
 
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
+  return { days, hours, minutes, seconds };
+}
+
+function handleShowClockClick() {
+  
+  setInterval(convertMs(differentTime), 1000);
+
+  const timerDays = document.querySelector('[data-days]');
+  const timerHours = document.querySelector('[data-hours]');
+  const timerMinutes = document.querySelector('[data-minutes]');
+  const timerSeconds = document.querySelector('[data-seconds]');
+
+  // timerDays.textContent = timeClock.days;
+  // timerHours.textContent = timeClock.hours;
+  // timerMinutes.textContent = timeClock.minutes;
+  // timerSeconds.textContent = timeClock.seconds;
+}
