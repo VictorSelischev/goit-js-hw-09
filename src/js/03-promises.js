19442c29643e406abf02381617c133bf7d8c01fc
Victor Selischev <selischev.victor.kharkov@gmail.com>
2022-08-08 16:58:06 +0300

    Подключает библиотеку Notiflix

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,5 @@
+import Notiflix from 'notiflix';
+
 const refs = {
   formRef: document.querySelector('.form'),
 };
@@ -34,10 +36,10 @@ function handleCreatePromisesSubmit(event) {
   for (let i = 1; i <= amountRefs; i += 1) {
     createPromise(i, delayRefs)
       .then(({ position, delay }) => {
-        console.log(`✅ Fulfilled promise ${position} in ${delay} ms`);
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay} ms`);
       })
       .catch(({ position, delay }) => {
-        console.log(`❌ Rejected promise ${position} in ${delay} ms`);
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay} ms`);
       });
 
     // console.log(i);
