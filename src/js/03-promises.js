de166e3ca0e580e23ce015f3757d7471dc483a1c
Victor Selischev <selischev.victor.kharkov@gmail.com>
2022-08-08 16:40:22 +0300

    Решает задачу 3 модуля 10

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
+const refs = {
+  formRef: document.querySelector('.form'),
+};
+
+refs.formRef.addEventListener('submit', handleCreatePromisesSubmit);
+
+
+// ======================================
+// FUNCTION
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function handleCreatePromisesSubmit(event) {
+  event.preventDefault();
+
+  let delayRefs = +event.currentTarget.elements.delay.value;
+  const amountRefs = +event.currentTarget.elements.amount.value;
+  const stepRefs = +event.currentTarget.elements.step.value;
+
+  for (let i = 1; i <= amountRefs; i += 1) {
+    createPromise(i, delayRefs)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay} ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay} ms`);
+      });
+
+    // console.log(i);
+    delayRefs += stepRefs;
+
   }
 }
